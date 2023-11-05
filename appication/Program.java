354d4cc4a3ce9e38f37dd71adc0315259e1e75d3
luis felipe cardoso marinho <126625142+luisfelipecad@users.noreply.github.com>
2023-11-05 15:28:39 -0300

    feat validation vs

---
+++ appication/Program.java
@@ -0,0 +1,16 @@
+package appication;
+
+import boadgame.Position;
+
+public class Program {
+    
+
+    public static void main(String[] args){
+
+
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
+}
+
+
